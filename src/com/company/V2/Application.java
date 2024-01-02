6f9fa19e16e14aa276bcee5bafd03146f89461f5
Esunsin <jinchan1148@gmail.com>
2024-01-02 22:53:17 +0900

    test

--- src/com/company/V2/Application.java
+++ src/com/company/V2/Application.java
@@ -2,6 +2,7 @@ package com.company.V2;
 
 public class Application {
     public static void main(String[] args) throws InterruptedException {
+        //test
         Play play = new Play();
         play.play();
     }
